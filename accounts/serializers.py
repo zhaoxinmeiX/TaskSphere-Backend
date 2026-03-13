f2dd6c2588c339d5cb2a8b69f6ae8991298db617
zhaoxinmei <541902876@qq.com>
2026-03-14 12:18:00 +1300

    Add login API endpoint for user authentication

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 
@@ -28,4 +29,27 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True, trim_whitespace=False)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            
+            if not user:
+                raise serializers.ValidationError('Invalid credentials.')
+            
+            if not user.is_active:
+                raise serializers.ValidationError('User account is disabled.')
+            
+            attrs['user'] = user
+            return attrs
+        else:
+            raise serializers.ValidationError('Both username and password are required.')
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
 
